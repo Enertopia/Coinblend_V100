96e159d11fd5c0ccbcfd8f082b5bacac7b7432dd
Emiliano G Solazzi Griminger <133587754+Enertopia@users.noreply.github.com>
2024-03-27 15:24:04 -0400

    Create secure_key_storage.py

---
+++ utils/secure_key_storage.py
@@ -0,0 +1,25 @@
+from cryptography.fernet import Fernet
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import os
+
+class SecureKeyStorage:
+    def __init__(self, password):
+        salt = os.urandom(16)
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+        self.fernet = Fernet(key)
+
+    def encrypt(self, data):
+        return self.fernet.encrypt(data.encode())
+
+    def decrypt(self, token):
+        return self.fernet.decrypt(token).decode()
